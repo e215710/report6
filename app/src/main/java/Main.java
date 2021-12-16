b150b8468879c98cad97f66c474bb0fde398345a
Fukamura Megu <e215710@ie.u-ryukyu.ac.jp>
2021-12-16 18:20:01 +0900

    PlayerクラスとEnemyクラス作成

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,5 +1,23 @@
+import jp.ac.uryukyu.ie.e215710.Enemy;
+import jp.ac.uryukyu.ie.e215710.Ninja;
+
 public class Main {
     public static void main(String[]args) {
+        System.out.println() ;
+        Ninja ninja = new Ninja("NARUTO", 30, 10, 10) ;
+        Enemy enemy = new Enemy("大蛇丸", 30, 5, 4) ;
 
+        System.out.println(ninja.getName() + " VS " + enemy.getName()) ;
+        System.out.println("果たしてどちらが勝つのだろうか・・・") ;
+        System.out.println() ;
+
+        int turn = 0 ;
+        while(ninja.getDeath() == "生" && enemy.getDeath()== "生") {
+            turn ++ ;
+            System.out.println(turn + "ターン目") ;
+            ninja.kunai(enemy) ;
+            enemy.syuriken(ninja) ;
+        }
+        System.out.println("激しい戦いは幕を閉じた") ;
     }
 }
--- app/src/main/java/jp/ac/uryukyu/ie/e215710/Character.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215710/Character.java
@@ -5,46 +5,65 @@ public class Character {
     int hp ;
     int chakra ;
     int attack ;
+    String death ;
 
     Character(String name, int hp, int chakra, int attack) {
         this.name = name ;
         this.hp = hp ;
         this.chakra = chakra ;
         this.attack = attack ;
+        this.death = "生" ;
         System.out.println(name + "の登場!!") ;
         System.out.println(name + "のHPは" + hp + "、チャクラは" + chakra + "である。") ;
+        System.out.println() ;
     }
 
     public String getName() {
         return this.name ;
     }
 
+    public String getDeath() {
+        return this.death ;
+    }
+
     public void kunai(Character cha) {
-        int damageKunai = (int)(Math.random() * attack * 1.2) ;
+        int damageKunai = (int) (attack * 1.2) ;
         if (hp > 0) {
-            cha.injuredKunai(damageKunai);
             System.out.println(name + "は" + cha.getName() + "にクナイで攻撃した！") ;
+            System.out.println(name +"は" + cha.getName() + "に" + damageKunai + "ダメージ与えた") ;
+            cha.injuredKunai(damageKunai);
+            if (cha.hp > 0) {
+                System.out.println(name + "のHPは" + hp + "、" + cha.getName() + "のHPは" + cha.hp) ;
+            }
+            System.out.println() ;
         }
     }
 
     public void syuriken(Character cha) {
-        int damageShuriken = (int)(Math.random() * attack) ;
+        int damageShuriken = attack ;
         if (hp > 0) {
-            cha.injuredShuriken(damageShuriken);
             System.out.println(name + "は" + cha.getName() + "に手裏剣で攻撃した！") ;
+            System.out.println(name +"は" + cha.getName() + "に" + damageShuriken + "ダメージ与えた") ;
+            cha.injuredShuriken(damageShuriken);
+            if (cha.hp > 0) {
+                System.out.println(name + "のHPは" + hp + "、" + cha.getName() + "のHPは" + cha.hp) ;
+            }
+            System.out.println() ;
         }
     }
 
     public void injuredKunai(int damageKunai) {
+        hp -= damageKunai ;
         if (hp <= 0) {
-            hp -= damageKunai ;
+            death = "死" ;
             System.out.println(name + "は力尽きた...") ;
         }
     }
 
     public void injuredShuriken(int damageShuriken) {
+        hp -= damageShuriken ;
         if (hp <= 0) {
-            hp -= damageShuriken ;
+            death = "死" ;
             System.out.println(name + "は力尽きた...") ;
         }
     }
