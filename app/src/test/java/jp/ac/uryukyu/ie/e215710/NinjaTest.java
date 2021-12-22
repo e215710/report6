c730d866fa1714d71b45873d07fbd65c51386198
Fukamura Megu <e215710@ie.u-ryukyu.ac.jp>
2021-12-22 14:55:44 +0900

    NinjaTestクラス作成

---
+++ app/src/test/java/jp/ac/uryukyu/ie/e215710/NinjaTest.java
@@ -0,0 +1,23 @@
+package jp.ac.uryukyu.ie.e215710;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class NinjaTest {
+    /**
+     * 忍者はチャクラの量が攻撃力の半分より少ないとき螺旋丸を出せないことを検証。
+     * （１）忍者と敵を準備。
+     * （２）忍者のチャクラは攻撃力の半分より少ないとする。
+     * （３）忍者のチャクラは攻撃力の半分より少ないため螺旋丸を出せないことを期待。
+     */
+
+    @Test
+    void rasenganTest() {
+        int defaultNinjaChakra = 4 ;
+        Ninja a = new Ninja("a", 30, defaultNinjaChakra, 10) ;
+        Enemy b = new Enemy("b", 30, 4, 10) ;
+        a.rasengan(b) ;
+        b.syuriken(a) ;
+        assertEquals(defaultNinjaChakra, a.getChakra()) ;
+    }
+}
