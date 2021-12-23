d675b03cc698fecf2333819cdc5fb67baf215a9d
Fukamura Megu <e215710@ie.u-ryukyu.ac.jp>
2021-12-23 16:56:30 +0900

    Enemyクラスのコメント文

--- app/src/main/java/jp/ac/uryukyu/ie/e215710/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215710/Enemy.java
@@ -5,6 +5,12 @@ public class Enemy extends Character {
         super(name, hp, chakra, attack) ;
     }
 
+    /**
+     * 自分自身にクナイで攻撃された時の、自分のHPが減るメソッド
+     * 自分自身のHPがattack(攻撃力)の1.2倍のダメージ減る
+     * 自分自身のHPが0以下になったらdeathの状態は死となる
+     * @param damageKunai
+     */
     @Override
     public void injuredKunai(int damageKunai) {
         hp -= damageKunai ;
@@ -14,6 +20,12 @@ public class Enemy extends Character {
         }
     }
 
+    /**
+     * 自分自身に手裏剣で攻撃された時の、自分のHPが減るメソッド
+     * HPがattack(攻撃力)のダメージ減る
+     * 自分自身のHPが0以下になったらdeathの状態は死となる
+     * @param damageKunai
+     */
     @Override
     public void injuredShuriken(int damageShuriken) {
         hp -= damageShuriken ;
@@ -23,6 +35,12 @@ public class Enemy extends Character {
         }
     }
 
+    /**
+     * 自分自身に螺旋丸で攻撃された時の、自分のHPが減るメソッド
+     * HPがattack(攻撃力)の2倍のダメージ減る
+     * 自分自身のHPが0以下になったらdeathの状態は死となる
+     * @param damageKunai
+     */
     public void injuredRasengan(int damageRasengan) {
         hp -= damageRasengan ;
         if (hp <= 0) {
